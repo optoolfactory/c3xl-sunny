50976c08450dc2910e6d321782fd5b1a407b2028
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2026-02-15 19:34:20 +0900

    Update selfdrived.py

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -86,6 +86,10 @@ class SelfdriveD:
     ignore = self.sensor_packets + self.gps_packets + ['alertDebug']
     if SIMULATION:
       ignore += ['driverCameraState', 'managerState']
+
+    self.camera_packets.remove("driverCameraState")
+    ignore += ['driverMonitoringState']
+    
     if REPLAY:
       # no vipc in replay will make them ignored anyways
       ignore += ['roadCameraState', 'wideRoadCameraState']
