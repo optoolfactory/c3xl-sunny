ddef5edea6398d22d56dec06543026957dcfea94
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2026-03-08 21:50:38 +0900

    Update registration.py

--- system/athena/registration.py
+++ system/athena/registration.py
@@ -23,6 +23,8 @@ def is_registered_device() -> bool:
   dongle = Params().get("DongleId")
   return dongle not in (None, UNREGISTERED_DONGLE_ID)
 
+DUMMY_IMEI1 = '865420071780982'
+DUMMY_IMEI2 = '865420071780983'
 
 def register(show_spinner=False, register_konik=False) -> str | None:
   """
@@ -90,8 +92,11 @@ def register(show_spinner=False, register_konik=False) -> str | None:
         backoff = min(backoff + 1, 15)
         time.sleep(backoff)
 
-      if time.monotonic() - start_time > 60 and show_spinner:
+      if time.monotonic() - start_time > 30 and show_spinner:
         spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
+        imei1 = DUMMY_IMEI1
+        imei2 = DUMMY_IMEI2
+        break
 
     if show_spinner:
       spinner.close()
